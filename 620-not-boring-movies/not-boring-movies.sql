5cc1771066cda7c869b22c1f4f8b379f22834f5b
Nishchay-jobs <nishchay.bn.jobs@gmail.com>
2024-07-12 15:25:58 -0400

    Time: 212 ms (84.38%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 select * from Cinema
-where MOD(id, 2) <> 0 AND description NOT LIKE 'boring'
+where MOD(id, 2) <> 0 AND description!='boring'
 ORDER BY rating desc
